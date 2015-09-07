61d0952f5a4cfbbfb91230e817ef8fa1076abba2
mickey <mickey75@gmail.com>
2015-09-07 13:16:23 +0200

    meta: enforce member name restriction

--- lib/PONAPI/Role/HasMeta.pm
+++ lib/PONAPI/Role/HasMeta.pm
@@ -5,6 +5,10 @@ use warnings;
 
 use Moose::Role;
 
+# decided not to support ' ' (space) as it's not recommended (not URL safe)
+my $re_member_name = qr{^[a-zA-Z0-9][a-zA-Z0-9_\-]*$};
+my $re_member_illegal_chars = qr{[+,.\[\]!'"#$%&()*/:;<=>?@\\^`{|}~]};
+
 has _meta => (
     init_arg => undef,
     traits   => [ 'Hash' ],
@@ -25,11 +29,16 @@ sub add_meta {
 
     while ( @args ) {
         my ($k, $v) = (shift @args, shift @args);
+
+        $k and !ref($k) and $k =~ /$re_member_name/ and $k !~ /$re_member_illegal_chars/
+            or die "[__PACKAGE__] add_meta: invalid member name: $k\n";
+
         $self->_meta->{$k} = $v;
     }
 
     return $self;
 }
+
 
 1;
 
