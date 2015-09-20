7346ec2ab0d28c707e3c2c47cb3c5b98af12e98f
Stevan Little <stevan.little@gmail.com>
2015-09-20 22:06:18 +0200

    just a note

--- lib/PONAPI/Builder/Role/HasLinksBuilder.pm
+++ lib/PONAPI/Builder/Role/HasLinksBuilder.pm
@@ -16,6 +16,13 @@ has 'links_builder' => (
 
 sub _build_links_builder { PONAPI::Builder::Links->new( parent => $_[0] ) }
 
+# NOTE:
+# These need to be delegated so that they 
+# can return the instance of the Builder 
+# they are attached to and not the Links
+# Builder itself.
+# - SL
+
 sub add_link {
     my ($self, @args) = @_;
     $self->links_builder->add_link( @args );
