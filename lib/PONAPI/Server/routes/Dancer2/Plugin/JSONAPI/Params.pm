333887fd48a8b9f99faaff78d2330fec3ba5f865
mickey <mickey75@gmail.com>
2015-10-09 16:11:10 +0200

    params plugin: force sort support config

--- lib/PONAPI/Server/routes/Dancer2/Plugin/JSONAPI/Params.pm
+++ lib/PONAPI/Server/routes/Dancer2/Plugin/JSONAPI/Params.pm
@@ -4,6 +4,12 @@ use Dancer2::Plugin;
 
 on_plugin_import {
     my $dsl = shift;
+
+    # force required jsonapi configuration
+    my $supports_sort = config->{jsonapi}{supports_sort};
+    defined $supports_sort and ( $supports_sort == 0 or $supports_sort == 1 )
+        or die "[JSONAPI] configuration missing: {jsonapi}{supports_sort}";
+
     $dsl->app->add_hook(
         Dancer2::Core::Hook->new(
             name => 'before',
@@ -13,12 +19,16 @@ on_plugin_import {
                 for my $k ( keys %{ $dsl->query_parameters } ) { # unique keys
                     my ( $p, $f ) = $k =~ /^ (\w+?) (?:\[(\w+)\])? $/x;
                     grep { $p eq $_ } qw< fields filter page include sort >
-                        or $dsl->send_error("[JSON-API] Bad request (unsupported parameters)", 400);
+                        or $dsl->send_error(
+                            "[JSON-API] Bad request (unsupported parameters)", 400
+                        );
 
-                    # TODO: this is implementation specific
-                    # take from conf?
-                    $p eq 'sort'
-                        and $dsl->send_error("[JSON-API] Server-side sorting not supported", 400);
+                    # sort support is set in config->{jsonapi}{supports_sort}
+                    if ( $p eq 'sort' and !$supports_sort ) {
+                        $dsl->send_error(
+                            "[JSON-API] Server-side sorting not supported", 400
+                        );
+                    }
 
                     my @values = map { split /,/ } $dsl->query_parameters->get_all($k);
                     $dsl->query_parameters->remove($k);
