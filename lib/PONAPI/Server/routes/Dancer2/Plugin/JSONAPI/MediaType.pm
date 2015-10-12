aa08d1e2acf24a10ae462565e0ca37f4cae7bb9c
mickey <mickey75@gmail.com>
2015-10-12 10:31:46 +0200

    plugins: corrected behavior + documentation

--- lib/PONAPI/Server/routes/Dancer2/Plugin/JSONAPI/MediaType.pm
+++ lib/PONAPI/Server/routes/Dancer2/Plugin/JSONAPI/MediaType.pm
@@ -3,33 +3,45 @@ package Dancer2::Plugin::JSONAPI::MediaType;
 use Dancer2::Plugin;
 
 my $jsonapi_mediatype = 'application/vnd.api+json';
+my $match_jsonapi_mt  = qr/^application\/vnd\.api\+json/;
 
 on_plugin_import {
     my $dsl = shift;
+
+    ### add 'before' hook to check type
 
     $dsl->app->add_hook(
         Dancer2::Core::Hook->new(
             name => 'before',
             code => sub {
-                # Content-Type is $jsonapi_mediatype
-                my $ct = $dsl->request->headers->{'content-type'}
-                    || $dsl->send_error("[JSON-API] missing Content-Type header", 415);
-                $ct eq $jsonapi_mediatype and return;
+                # enforce json-api Content-Type (no extra params)
+                my $ct = $dsl->request->headers->{'content-type'};
+                $ct and $ct eq $jsonapi_mediatype
+                    or $dsl->send_error(
+                        "[JSON-API] missing Content-Type header", 415
+                    );
 
-                # if it contains params --> error 415
-                substr( $ct, 0, length($jsonapi_mediatype) ) eq $jsonapi_mediatype
-                    and $dsl->send_error("[JSON-API] Content-Type is not $jsonapi_mediatype", 415);
+                # if json-api is sent in Accept headers,
+                # at least one instance must not contain any params
 
-                # alternatively, we have it in Accept (at least once clean with no parameters)
-                my @accept = split /,/ => $dsl->request->headers->{'accept'};
-                grep { $_ eq $jsonapi_mediatype } @accept
-                    or $dsl->send_error("[JSON-API] Accept doesn't contain $jsonapi_mediatype entry", 406);
+                my @accept = grep { /$match_jsonapi_mt/ }
+                    split /,/ => $dsl->request->headers->{'accept'};
+
+                if ( @accept > 0 ) {
+                    grep { $_ eq $jsonapi_mediatype } @accept
+                        or $dsl->send_error(
+                            "[JSON-API] Accept header contains only parameterized instances of json-api", 406
+                        );
+                }
 
                 # all good
                 return;
             },
         )
     );
+
+
+    ### add 'after' hook to force content-type
 
     $dsl->app->add_hook(
         Dancer2::Core::Hook->new(
--- lib/PONAPI/Server/routes/Dancer2/Plugin/JSONAPI/Params.pm
+++ lib/PONAPI/Server/routes/Dancer2/Plugin/JSONAPI/Params.pm
@@ -5,10 +5,15 @@ use Dancer2::Plugin;
 on_plugin_import {
     my $dsl = shift;
 
-    # force required jsonapi configuration
+    ### read jsonapi configuration
+
+    # force explicit setting of 'sort' support configuration
     my $supports_sort = $dsl->config->{jsonapi}{supports_sort};
     defined $supports_sort and ( $supports_sort == 0 or $supports_sort == 1 )
-        or die "[JSONAPI] configuration missing: {jsonapi}{supports_sort}";
+        or die "[JSON-API] configuration missing: {jsonapi}{supports_sort}";
+
+
+    ### add 'before' hook
 
     $dsl->app->add_hook(
         Dancer2::Core::Hook->new(
@@ -16,30 +21,39 @@ on_plugin_import {
             code => sub {
                 my %params = ( fields => {}, filter => {}, page => {}, include => {} );
 
-                for my $k ( keys %{ $dsl->query_parameters } ) { # unique keys
+                # loop over query parameters (unique keys)
+                for my $k ( keys %{ $dsl->query_parameters } ) {
                     my ( $p, $f ) = $k =~ /^ (\w+?) (?:\[(\w+)\])? $/x;
+
+                    # valid parameter names
                     grep { $p eq $_ } qw< fields filter page include sort >
                         or $dsl->send_error(
                             "[JSON-API] Bad request (unsupported parameters)", 400
                         );
 
-                    # sort support is set in config->{jsonapi}{supports_sort}
+                    # 'sort' requested but not supported
                     if ( $p eq 'sort' and !$supports_sort ) {
                         $dsl->send_error(
                             "[JSON-API] Server-side sorting not supported", 400
                         );
                     }
 
+                    # values can be passed as CSV
                     my @values = map { split /,/ } $dsl->query_parameters->get_all($k);
+
+                    # remove original values
                     $dsl->query_parameters->remove($k);
 
+                    # values passed on in array-ref
                     grep { $p eq $_ } qw< fields filter page >
                         and $params{$p}{$f} = \@values;
 
+                    # values passed on in hash-ref
                     $p eq 'include'
                         and $params{include}{$_} = 1 for @values;
                 }
 
+                # add manipulated parameters structure back
                 $dsl->query_parameters->add( $_, $params{$_} ) for keys %params;
             },
         )
