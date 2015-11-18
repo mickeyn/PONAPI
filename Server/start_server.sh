193fefada0f2eb3ba4a397969165bba72f98663c
mickey <mickey75@gmail.com>
2015-11-18 10:21:12 +0100

    move Dancer2 server to its own dir (to allow alternative implementations under Server)

--- Server/start_server.sh
+++ Server/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # requires Dancer2 version 0.163+
-plackup -r -Ilib -Ilib/PONAPI/Server/routes/ -e'use PONAPI; PONAPI->to_app'
+plackup -r -Ilib -Ilib/PONAPI/Server/Dancer2/routes/ -e'use PONAPI; PONAPI->to_app'
