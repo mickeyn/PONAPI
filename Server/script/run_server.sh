32bad7b619a091e50fb71de72e667a0e4cdf5c9d
mickey <mickey75@gmail.com>
2015-12-08 10:19:02 +0100

    support X-HTTP-Method-Override

--- Server/script/run_server.sh
+++ Server/script/run_server.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-plackup -R conf -Ilib -e'use PONAPI::Server; PONAPI::Server->to_app'
+plackup \
+  -R conf \
+  -MPlack::Middleware::MethodOverride \
+  -Ilib \
+  -e 'use PONAPI::Server; my $app = PONAPI::Server->to_app; Plack::Middleware::MethodOverride->wrap($app);'
