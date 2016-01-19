37467d054afd0c7f4052de76d3be019d4e38b990
mickey <mickey75@gmail.com>
2016-01-19 16:51:22 +0100

    Server: release 0.002005

--- Server/lib/PONAPI/Server.pm
+++ Server/lib/PONAPI/Server.pm
@@ -1,7 +1,7 @@
 # ABSTRACT: PONAPI - Perl implementation of {JSON:API} (http://jsonapi.org/) v1.0
 package PONAPI::Server;
 
-our $VERSION = '0.002004';
+our $VERSION = '0.002005';
 
 use Plack::Request;
 use Plack::Response;
